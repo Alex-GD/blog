34f907293770a8782cbff2e69cc26f97d4f4c6aa
Alejandro <desarrollo@satekactivos.com>
2022-12-22 13:23:56 +0100

    sub

--- src/site-meta.config.ts
+++ src/site-meta.config.ts
@@ -1,8 +1,8 @@
 const siteMeta = {
-  title: "Astro Theme Cactus",
-  description: "An opinionated starter theme for Astro",
-  lang: "en-GB",
-  ogLocale: "en_GB",
+  title: "Blog",
+  description: "Blog personal realizado con Astro",
+  lang: "es-ES",
+  ogLocale: "es_ES",
   githubUrl: "https://github.com/chrismwilliams/astro-theme-cactus",
   themeColorLight: "#fafafa",
   themeColorDark: "#1d1f21",
